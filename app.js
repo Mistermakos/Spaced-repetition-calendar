782299ea68d75405918ede9426c0b10abb48f701
Mistermakos <mistermakos.mcg@gmail.com>
2023-09-12 23:22:53 +0200

    I realized that app.static exist and made changes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const app = express()
 const Handlers = require("./backend/handlers")
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/frontend`))
 
 app.route("/").get((req, res) => getHome(req,res))
 
-app.route("/:id").get((req,res) => getInfo(req,res))
-
 app.route("/api/v1/Date").get((req,res) => {getDate(res)})
 
-app.listen(3000, () => {console.log("Server wors on http://localhost:3000");})
+module.exports = app;
--- backend/handlers.js
+++ backend/handlers.js
@@ -1,20 +1,9 @@
 const fs = require("fs")
 const HomePage = fs.readFileSync("frontend/index.html")
-const date_fetcher = fs.readFileSync("frontend/date_fetcher.js")
 
 module.exports = getHome = (req,res) => 
 {
     res.end(HomePage)
-}
-
-module.exports = getInfo = (req,res) =>
-{
-    let info = req.path
-    if(info.includes(".js"))
-    {
-        info = info.slice(1,info.length-3)
-        res.end(eval(info))
-    }
 }
 
 module.exports = getDate = (res) =>
